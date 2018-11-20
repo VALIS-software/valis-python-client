4e7790b61bf86c6e70312e9c3ca79e1c0bf40b0a
Salik Syed <ssyed@cs.stanford.edu>
2018-11-19 22:46:17 -0600

    update docs

--- examples/immune_atlas.py
+++ examples/immune_atlas.py
@@ -1,21 +1,20 @@
 from valis import valis, Dataset
 
-
-
-# generate query for traits in GWAS Catalog containing text 
+# write a query for traits
 raTraits = valis.traits.search('rheumatoid arthritis', [Dataset.GWAS_CATALOG])
 ucTraits = valis.traits.search('ulcerative colitis', [Dataset.GWAS_CATALOG])
 
-# variants
+# write a query for variants data
 dbsnp = valis.variants.query(datasets=[Dataset.DBSNP])
 
-# gwas
+# gwas takes in a variantQuery and a traitQuery
 raSNPs = valis.variants.gwas(maxPValue=0.01, variantQuery=dbsnp, traitQuery=raTraits, gwasDatasets=[Dataset.GWAS_CATALOG])
 ucSNPs = valis.variants.gwas(maxPValue=0.01, variantQuery=dbsnp, traitQuery=ucTraits, gwasDatasets=[Dataset.GWAS_CATALOG])
 
-
+# intersect the two SNPs
 both = raSNPs.intersect(ucSNPs)
 
+# fetch the immune atlas annotations
 immune = valis.annotations.query(datasets=Dataset.IMMUNEATLAS, biosamples='1002-Th1_precursors-U')
 
 # to fetch all metadata set full=True
