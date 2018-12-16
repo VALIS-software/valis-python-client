6003b4175e357cddbadb3d74acbbdf62608d98c9
qyd <saviorbruce@gmail.com>
2018-12-15 23:53:24 -0800

    fix bugs in QueryBuilder about limit

--- valis/QueryBuilder.py
+++ valis/QueryBuilder.py
@@ -1,5 +1,5 @@
 import json
-from .QueryType import *
+from .QueryType import QueryType
 
 class QueryBuilder:
   def __init__(self, api):
@@ -47,13 +47,13 @@ class QueryBuilder:
       value = value[0]
     if (type(value) == list):
       if value[0] == 'AND':
-        self.query['filters'][filterKey] = { '$all' : value[1:] };
+        self.query['filters'][filterKey] = { '$all' : value[1:] }
       elif value[0] == 'OR':
-        self.query['filters'][filterKey] = { '$in' : value[1:] };
+        self.query['filters'][filterKey] = { '$in' : value[1:] }
       else:
-        self.query['filters'][filterKey] = { '$in' : value };
+        self.query['filters'][filterKey] = { '$in' : value }
     else:
-      self.query['filters'][filterKey] = value;
+      self.query['filters'][filterKey] = value
     return self
 
   def setUserFileID(self, userFileID):
@@ -61,51 +61,51 @@ class QueryBuilder:
     copy.query['userFileID'] = userFileID
     return copy
 
-  def filterID(self, id):
+  def filterID(self, doc_id):
     copy = self.duplicate()
-    copy.query['filters']['_id'] = id;
+    copy.query['filters']['_id'] = doc_id
     return copy
 
   def filterType(self, types):
     copy = self.duplicate()
     copy.setFilterValue('type', types)
     return copy
-  
+
   def filterSource(self, source):
     copy = self.duplicate()
     copy.setFilterValue('source', source)
     return copy
-  
+
   def filterContig(self, contig):
     if (self.query['type'] != QueryType.GENOME):
       raise Exception('filter contig only available for GenomeNodes')
     copy = self.duplicate()
     copy.setFilterValue('contig', contig)
     return copy
-  
+
   def filterLength(self, length):
     if (self.query.type != QueryType.GENOME):
       raise Exception('Length only available for GenomeNodes')
-    
+
     copy = self.duplicate()
-    copy.query['filters']['length'] = length;
+    copy.query['filters']['length'] = length
     return copy
-  
+
   def filterName(self, name):
     copy = self.duplicate()
-    copy.setFilterValue('name', name);
+    copy.setFilterValue('name', name)
     return copy
-  
+
   def filterPathway(self, pathways):
     copy = self.duplicate()
-    copy.setFilterValue('info.kegg_pathways', pathways);
+    copy.setFilterValue('info.kegg_pathways', pathways)
     return copy
-  
+
   def filterMaxPValue(self, pvalue):
     copy = self.duplicate()
-    copy.query['filters']['info.p-value'] = { '<': pvalue };
+    copy.query['filters']['info.p-value'] = { '<': pvalue }
     return copy
-  
+
   def filterBiosample(self, biosample):
     copy = self.duplicate()
     if biosample == None:
@@ -114,63 +114,63 @@ class QueryBuilder:
       biosample = [biosample]
     copy.setFilterValue('info.biosample', biosample)
     return copy
-    
+
   def filterTargets(self, targets):
     copy = self.duplicate()
     if targets == None:
       return copy
     if len(targets):
-      copy.query['filters']['info.targets'] = { '$all': targets };
+      copy.query['filters']['info.targets'] = { '$all': targets }
     return copy
-    
+
   def filterAnnotationType(self, annotationType):
     copy = self.duplicate()
-    copy.setFilterValue('info.types', annotationType);
+    copy.setFilterValue('info.types', annotationType)
     return copy
-  
+
   def filterAssay(self, assay):
     copy = self.duplicate()
-    copy.setFilterValue('info.assay',assay);
+    copy.setFilterValue('info.assay',assay)
     return copy
-  
+
   def filterOutType(self, outType):
     copy = self.duplicate()
-    copy.setFilterValue('info.outtype', outType);
+    copy.setFilterValue('info.outtype', outType)
     return copy
-  
+
   def filterVitalStatus(self, vitalStatus):
     copy = self.duplicate()
-    copy.setFilterValue('info.vital_status', vitalStatus);
+    copy.setFilterValue('info.vital_status', vitalStatus)
     return copy
 
   def filterPatientBarCode(self, outType):
     copy = self.duplicate()
-    copy.setFilterValue('info.patient_barcodes', outType);
+    copy.setFilterValue('info.patient_barcodes', outType)
     return copy
 
   def filterPatientGender(self, gender):
     copy = self.duplicate()
-    copy.setFilterValue('info.gender', gender);
+    copy.setFilterValue('info.gender', gender)
     return copy
 
   def filterPatientDisease(self, disease_code):
     copy = self.duplicate()
-    copy.setFilterValue('info.disease_code', disease_code);
+    copy.setFilterValue('info.disease_code', disease_code)
     return copy
-  
+
   def filterStartBp(self, start):
     if self.query['type'] != QueryType.GENOME:
       raise Exception('filterStartBp is only available for an Genome Query.')
-    
+
     copy = self.duplicate()
-    copy.query['filters']['start'] = start;
+    copy.query['filters']['start'] = start
     return copy
-  
+
   def filterEndBp(self, end):
     if self.query['type'] != QueryType.GENOME:
       raise Exception('filterEndBp is only available for an Genome Query.')
     copy = self.duplicate()
-    copy.query['filters']['end'] = end;
+    copy.query['filters']['end'] = end
     return copy
 
   def filterRegion(self, contig, start, end):
@@ -179,13 +179,13 @@ class QueryBuilder:
     copy.query['filters']['start'] = { '>' : start }
     copy.query['filters']['end'] = { '<' : end }
     return copy
-  
+
   def filterAffectedGene(self, gene):
-    previous = self.query['filters']['variant_affected_genes'] or [];
+    previous = self.query['filters']['variant_affected_genes'] or []
     copy = self.duplicate()
-    copy.query['filters']['info.variant_affected_genes'] = gene;
+    copy.query['filters']['info.variant_affected_genes'] = gene
     return copy
-  
+
   def filterVariantTag(self, tags):
     copy = self.duplicate()
     copy.setFilterValue('info.variant_tags',  tags)
@@ -193,12 +193,12 @@ class QueryBuilder:
 
   def searchText(self, text):
     copy = self.duplicate()
-    copy.query['filters']['$text'] = text;
+    copy.query['filters']['$text'] = text
     return copy
 
   def setLimit(self, limit):
     copy = self.duplicate()
-    copy.query['limit'] = limit;
+    copy.query['limit'] = limit
     return copy
 
   def get(self):
@@ -206,7 +206,7 @@ class QueryBuilder:
 
   def json(self):
     return json.dumps(self.query)
- 
+
   def __str__(self):
     return json.dumps(self.query)
 
@@ -214,7 +214,7 @@ class QueryBuilder:
     if (self.query['type'] == QueryType.EDGE):
       raise Exception('Edge can not be connected to another edge.')
     copy = self.duplicate()
-    copy.query['toEdges'].append(edgeQuery.get());
+    copy.query['toEdges'].append(edgeQuery.get())
     if (not 'toNode' in edgeQuery.get()):
       copy.isEdgeOnly = True
     return copy
@@ -223,8 +223,8 @@ class QueryBuilder:
     if (self.query['type'] != QueryType.EDGE):
       raise Exception('toNode is only available for an Edge Query.')
     copy = self.duplicate()
-    copy.query['toNode'] = nodeQuery.get();
-    copy.query['reverse'] = reverse;
+    copy.query['toNode'] = nodeQuery.get()
+    copy.query['reverse'] = reverse
     return copy
 
 
@@ -239,7 +239,7 @@ class QueryBuilder:
       ar['windowSize'] = int(windowSize)
       ar['operator'] = 'window'
     copy = self.duplicate()
-    copy.query['arithmetics'].append(ar);
+    copy.query['arithmetics'].append(ar)
     return copy
 
   def union(self, queries):
@@ -252,7 +252,7 @@ class QueryBuilder:
       'target_queries': queries,
     }
     copy = self.duplicate()
-    copy.query['arithmetics'].append(ar);
+    copy.query['arithmetics'].append(ar)
     return copy
 
   def diff(self, queries):
@@ -265,13 +265,12 @@ class QueryBuilder:
       'target_queries': queries,
     }
     copy = self.duplicate()
-    copy.query['arithmetics'].append(ar);
+    copy.query['arithmetics'].append(ar)
     return copy
 
   def fetch(self, full=False, startIdx=None, endIdx=None, limit=None):
-    if not limit:
-      limit = 1000000
-    result, has_more = self.api.getQueryResults(self.setLimit(limit), full, startIdx, endIdx)
+    query = self.setLimit(limit) if limit is not None else self
+    result, _ = self.api.getQueryResults(query, full, startIdx, endIdx)
     return result
 
   def release(self):
@@ -279,7 +278,7 @@ class QueryBuilder:
       self.api.deleteFile(self.query['userFileID'])
     else:
       raise Exception('Cannot release query (not a user generated data file)')
-  
+
   def distinctValues(self, key):
     return self.api.distinctValues(key, self)
 
